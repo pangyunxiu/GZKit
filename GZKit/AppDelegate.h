d3e9f0db8e5c760fbeab2af9f9b37821a919f9da
Gang679 <3139726292@qq.com>
2017-03-22 11:45:54 +0800

    Initial Commit

---
+++ GZKit/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GZKit
+//
+//  Created by xinshijie on 17/3/22.
+//  Copyright © 2017年 Mr.quan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GZKit/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GZKit
+//
+//  Created by xinshijie on 17/3/22.
+//  Copyright © 2017年 Mr.quan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
